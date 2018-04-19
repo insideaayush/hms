c6d6751cedc90eed2f69037aec63e7a3baed9cde
Aayush Gautam <aayush.gautam924@gmail.com>
2018-04-19 15:16:18 +0530

    added new date time component

---
+++ frontend/src/components/DateTimePicker.js
@@ -0,0 +1,23 @@
+import React, { Fragment, PureComponent } from 'react';
+import DTP from 'material-ui-pickers/DateTimePicker';
+
+class DateTimePicker extends PureComponent {
+    render() {
+        return (
+            <Fragment>
+                <div className="picker">
+                    <DTP
+                        value={this.props.selectedDate}
+                        disablePast
+                        onChange={this.props.handleDateChange}
+                        label="Select Appointment Time"
+                        showTodayButton
+                        fullWidth
+                    />
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default DateTimePicker
